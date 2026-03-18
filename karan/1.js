08a4e459036b8a741bbc847ba8378952dcbe7111
karan <aayushrawa66t@gmail.com>
2026-03-18 15:59:45 +0530

    down page

--- karan/1.js
+++ karan/1.js
@@ -1,7 +1,7 @@
-var typed = new typed(".text", {
-    String:["Fontend Developer" , "Web Developer"],
-    typeSpeed:100,
-    backspeed:100,
-    backDelay:1000,
-    loop:true
-}); 
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
